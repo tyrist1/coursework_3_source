fae5bcb949a79b7241c32ea470a1945d62cc0a43
Andrei <tyrist84@mail.ru>
2022-03-06 23:31:52 +0300

    добавил схеиу юзера

---
+++ project/schemas/user.py
@@ -0,0 +1,11 @@
+from marshmallow import fields, Schema
+
+
+class UserSchema(Schema):
+    id = fields.Int(required=True)
+    email =  fields.Str(required=True)
+    password =  fields.Str(required=True)
+    name =  fields.Str()
+    surname =  fields.Str()
+    favorite_genre_id =  fields.Int()
+
