b587649c471e898b48d84cca3e72fe8a6e4244b3
Andrei <tyrist84@mail.ru>
2022-03-06 23:26:34 +0300

    добавил функции к работе с моделями

--- project/dao/models/favorite_movie_del.py
+++ project/dao/models/favorite_movie_del.py
@@ -1,23 +1,23 @@
-from project.dao.models.base import BaseMixin
-from project.setup_db import db
-
-
-class Genre(BaseMixin, db.Model):
-    __tablename__ = "genres"
-
-    name = db.Column(db.String(100), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f"<Genre '{self.name.title()}'>"
-
-from setup_db import db
-
-
-class Genre(db.Model):
-    __tablename__ = 'genre'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f"<Genre '{self.name.title()}'>"
+# from project.dao.models.base import BaseMixin
+# from project.setup_db import db
+#
+#
+# class Genre(BaseMixin, db.Model):
+#     __tablename__ = "genres"
+#
+#     name = db.Column(db.String(100), unique=True, nullable=False)
+#
+#     def __repr__(self):
+#         return f"<Genre '{self.name.title()}'>"
+#
+# from setup_db import db
+#
+#
+# class Genre(db.Model):
+#     __tablename__ = 'genre'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), unique=True, nullable=False)
+#
+#     def __repr__(self):
+#         return f"<Genre '{self.name.title()}'>"
 
--- project/dao/movie.py
+++ project/dao/movie.py
@@ -1,14 +1,58 @@
 from sqlalchemy.orm.scoping import scoped_session
 
-from project.dao.models import Director
+from project.dao.models import Movie
 
 
-class DirectorDAO:
-    def __init__(self, session: scoped_session):
-        self._db_session = session
+class MovieDAO:
+    def __init__(self,  session: scoped_session):
+        self._db__db_session =  session
 
     def get_by_id(self, pk):
-        return self._db_session.query(Director).filter(Director.id == pk).one_or_none()
+        return self._db_session.query(Movie).filter(Movie.id == pk).one_or_none()
 
     def get_all(self):
-        return self._db_session.query(Director).all()
+        return self._db__db_session.query(Movie).all()
+    
+    def get_by_director_id(self, val):
+        return self._db_session.query(Movie).filter(Movie.director_id == val).all()
+
+    def get_by_genre_id(self, val):
+        return self._db_session.query(Movie).filter(Movie.genre_id == val).all()
+
+    def get_by_year(self, val):
+        return self._db_session.query(Movie).filter(Movie.year == val).all()
+
+    def create(self, movie_d):
+        ent = Movie(**movie_d)
+        self._db_session.add(ent)
+        self._db_session.commit()
+        return ent
+
+    def delete(self, rid):
+        movie = self.get_one(rid)
+        self._db_session.delete(movie)
+        self._db_session.commit()
+
+    def update(self, movie_d):
+        obj = self.get_by_id(movie_d.get('id'))
+        if obj:
+            if movie_d.get('title'):
+                obj.title = movie_d.get('title')
+            if movie_d.get('description'):
+                obj.description = movie_d.get('description')
+            if movie_d.get('trailer'):
+                obj.trailer = movie_d.get('trailer')
+            if movie_d.get('year'):
+                obj.year = movie_d.get('year')
+            if movie_d.get('rating'):
+                obj.rating = movie_d.get('rating')
+            if movie_d.get('genre_id'):
+                obj.genre_id = movie_d.get('genre_id')
+            if movie_d.get('director_id'):
+                obj.director_id = movie_d.get('director_id')
+
+
+            self._db_session.add(movie_d)
+            self._db_session.commit()
+            return obj
+        return "фильм не обновлялся"
